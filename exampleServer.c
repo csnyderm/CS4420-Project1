7b65535601bc01f54a9595c8bcb2226929569714
Cameron Livingston <89604510+KcLivings@users.noreply.github.com>
2023-02-28 15:14:56 -0700

    removed excess spaces, and reinserted _'s back into definitions

--- exampleServer.c
+++ exampleServer.c
@@ -7,30 +7,30 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-#define SERVER PORT 8080                             /* arbitrar y, but client & server must agree */
-#define BUF SIZE 4096                                /* block transfer size */
-#define QUEUE SIZE 10
+#define SERVER_PORT 8080                             /* arbitrar y, but client & server must agree */
+#define BUF_SIZE 4096                                /* block transfer size */
+#define QUEUE_SIZE 10
 
 int main(int argc, char *argv[])
 { int s, b, l, fd, sa, bytes, on = 1;
-  char buf[BUF SIZE];                                /* buffer for outgoing file */
-  str uct sockaddr in channel;                       /* holds IP address */
+  char buf[BUF_SIZE];                                /* buffer for outgoing file */
+  struct sockaddr_in_channel;                       /* holds IP address */
 
   /* Build address structure to bind to socket. */
   memset(&channel, 0, sizeof(channel));              /* zero channel */
-  channel.sin family = AF INET;
-  channel.sin addr.s addr = htonl(INADDR ANY);
-  channel.sin por t = htons(SERVER PORT);
+  channel.sin family = AF_INET;
+  channel.sin addr.s_addr = htonl(INADDR_ANY);
+  channel.sin port = htons(SERVER_PORT);
 
   /* Passive open. Wait for connection. */
-  s = socket(AF INET, SOCK STREAM, IPPROTO TCP);        /* create socket */
+  s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);        /* create socket */
   if (s < 0) {printf("socket call failed0); exit(-1);}
-  setsockopt(s, SOL SOCKET, SO REUSEADDR, (char *) &on, sizeof(on));
+  setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char *) &on, sizeof(on));
   
-  b = bind(s, (str uct sockaddr *) &channel, sizeof(channel));
+  b = bind(s, (struct sockaddr *) &channel, sizeof(channel));
   if (b < 0) {printf("bind failed0); exit(-1);}
   
-  l = listen(s, QUEUE SIZE);                         /* specify queue size */
+  l = listen(s, QUEUE_SIZE);                         /* specify queue size */
   if (l < 0) {printf("listen failed0); exit(-1);}
   
   /* Socket is now set up and bound. Wait for connection and process it. */
@@ -38,17 +38,17 @@ int main(int argc, char *argv[])
       sa = accept(s, 0, 0);                          /* block for connection request */
       if (sa < 0) {printf("accept failed0); exit(-1);}
       
-      read(sa, buf, BUF SIZE);                       /* read file name from socket */
+      read(sa, buf, BUF_SIZE);                       /* read file name from socket */
       
       /* Get and return the file. */
-      fd = open(buf, O RDONLY);                      /* open the file to be sent back */
+      fd = open(buf, O_RDONLY);                      /* open the file to be sent back */
       if (fd < 0) {printf("open failed");
 
       while (1) {
 
-           bytes = read(fd, buf, BUF SIZE); /* read from file */
+           bytes = read(fd, buf, BUF_SIZE); /* read from file */
            if (bytes <= 0) break;                    /* check for end of file */
-           wr ite(sa, buf, bytes);                   /* wr ite bytes to socket */
+           write(sa, buf, bytes);                   /* wr ite bytes to socket */
       }
       close(fd);                                     /* close file */
       close(sa);                                     /* close connection */
