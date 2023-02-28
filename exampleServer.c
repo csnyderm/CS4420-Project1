4367f555409052840847aea0bff3cfaa453d7bb6
Cameron Livingston <89604510+KcLivings@users.noreply.github.com>
2023-02-28 15:20:09 -0700

    reinserted quotations that were deleted on paste

--- exampleServer.c
+++ exampleServer.c
@@ -24,19 +24,19 @@ int main(int argc, char *argv[])
 
   /* Passive open. Wait for connection. */
   s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);        /* create socket */
-  if (s < 0) {printf("socket call failed0); exit(-1);}
+  if (s < 0) {printf("socket call failed"); exit(-1);}
   setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char *) &on, sizeof(on));
   
   b = bind(s, (struct sockaddr *) &channel, sizeof(channel));
-  if (b < 0) {printf("bind failed0); exit(-1);}
+  if (b < 0) {printf("bind failed"); exit(-1);}
   
   l = listen(s, QUEUE_SIZE);                         /* specify queue size */
-  if (l < 0) {printf("listen failed0); exit(-1);}
+  if (l < 0) {printf("listen failed"); exit(-1);}
   
   /* Socket is now set up and bound. Wait for connection and process it. */
   while (1) {
       sa = accept(s, 0, 0);                          /* block for connection request */
-      if (sa < 0) {printf("accept failed0); exit(-1);}
+      if (sa < 0) {printf("accept failed"); exit(-1);}
       
       read(sa, buf, BUF_SIZE);                       /* read file name from socket */
       
